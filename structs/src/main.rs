889e936b62dd9ebd00be0ab8bb4698a8488d984e
Peter Lyons <pete@peterlyons.com>
2017-05-01 16:57:38 -0600

    area with tuple

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -7,12 +7,12 @@
 //     let a2 = Album { year: 1999 };
 //     println!("Album {}", a2.year);
 // }
-fn area(length: u32, width: u32) -> u32 {
-    length * width
+fn area(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
 
 fn main() {
     let length1: u32 = 32;
     let width1: u32 = 42;
-    println!("The area is {}", area(length1, width1));
+    println!("The area is {}", area((length1, width1)));
 }
