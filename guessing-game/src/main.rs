d0cf17324d81b28818a08961c22ef35986d8eed0
Peter Lyons <pete@peterlyons.com>
2017-05-05 15:03:02 -0600

    Custom Guess type

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -3,6 +3,22 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
+struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess must be between 1 and 100, got {}", value);
+        }
+        Guess { value: value }
+    }
+
+    pub fn value(&self) -> u32 {
+        self.value
+    }
+}
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
@@ -19,9 +35,9 @@ fn main() {
             Err(_) => continue,
         };
         println!("You guessed: {}", guess);
+        let guess: Guess = Guess::new(guess);
 
-
-        match guess.cmp(&secret_number) {
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too low"),
             Ordering::Greater => println!("Too high"),
             Ordering::Equal => {
