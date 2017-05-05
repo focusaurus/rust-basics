7d4595dc35df86929c35817708e178f11ee4d039
Peter Lyons <pete@peterlyons.com>
2017-05-05 15:31:54 -0600

    largest not generic

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,4 +1,4 @@
-fn largest(numbers: &[i32]) -> i32 {
+fn largest_i32(numbers: &[i32]) -> i32 {
     let mut result = numbers[0];
     for &number in numbers.iter() {
         if number > result {
@@ -8,7 +8,19 @@ fn largest(numbers: &[i32]) -> i32 {
     result
 }
 
+fn largest_char(chars: &[char]) -> char {
+    let mut result = chars[0];
+    for &character in chars.iter() {
+        if character > result {
+            result = character
+        }
+    }
+    result
+}
+
 fn main() {
-    println!("largest {}", largest(&vec![7, 19, 23, 5]));
-    println!("largest {}", largest(&vec![]));
+    println!("largest {}", largest_i32(&vec![7, 19, 23, 5]));
+    println!("largest {}", largest_i32(&vec![67, 3, 99, 101]));
+    println!("largest {}", largest_char(&vec!['a', 'z', '&', '~']));
+    println!("largest {}", largest_char(&vec!['x', 'c']));
 }
