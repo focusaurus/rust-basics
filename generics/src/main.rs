9806088a4535c13ebc33fd327b822773a4afc8e0
Peter Lyons <pete@peterlyons.com>
2017-05-17 20:22:34 -0600

    generics working largest function

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -17,21 +17,21 @@ impl<T, U> Point<T, U> {
     }
 }
 
-// fn largest<T>(numbers: &[T]) -> T {
-//     let mut result = numbers[0];
-//     for &number in numbers.iter() {
-//         if number > result {
-//             result = number
-//         }
-//     }
-//     result
-// }
+fn largest<T: PartialOrd + Copy>(numbers: &[T]) -> T {
+    let mut result = numbers[0];
+    for &number in numbers.iter() {
+        if number > result {
+            result = number
+        }
+    }
+    result
+}
 
 fn main() {
-    // println!("largest {}", largest(&vec![7, 19, 23, 5]));
-    // println!("largest {}", largest(&vec![67, 3, 99, 101]));
-    // println!("largest {}", largest(&vec!['a', 'z', '&', '~']));
-    // println!("largest {}", largest(&vec!['x', 'c']));
+    println!("largest {}", largest(&vec![7, 19, 23, 5]));
+    println!("largest {}", largest(&vec![67, 3, 99, 101]));
+    println!("largest {}", largest(&vec!['a', 'z', '&', '~']));
+    println!("largest {}", largest(&vec!['x', 'c']));
     // let p = Point { x: 42, y: 43 };
     let p1 = Point { x: 7, y: 42.5 };
     let p2 = Point { x: "X", y: 'y' };
