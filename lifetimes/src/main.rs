acc5b7dc3806aebebbfa9190a31e24a32c44a5ad
Peter Lyons <pete@peterlyons.com>
2017-05-17 20:40:01 -0600

    lifetimes2

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,10 +1,13 @@
 fn longest<'a>(a: &'a str, b: &'a str) -> &'a str {
-    b
+    if a.len() >= b.len() {
+        return a;
+    }
+    return b;
 }
 
 fn main() {
-    let string1 = String::from("one");
-    let string2 = "two222";
-    let result = longest(string1.as_str(), string2);
+    let string1 = String::from("one1111");
+    let string2 = String::from("two22222");
+    let result = longest(string1.as_str(), string2.as_str());
     println!("{:?}", result);
 }
