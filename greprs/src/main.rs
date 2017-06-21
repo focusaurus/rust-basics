8be7ac43cf407d28626e311853ceaa570b9f1c0b
Peter Lyons <pete@peterlyons.com>
2017-06-21 11:20:04 -0600

    greprs run Result

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -1,9 +1,10 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
-use std::path::Path;
-use std::ffi::OsStr;
+// use std::path::Path;
+// use std::ffi::OsStr;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,16 +13,21 @@ fn main() {
                                                    process::exit(1);
                                                });
     println!("{:?}", args);
+    if let Err(err) = run(conf) {
+        println!("{}", err);
+        process::exit(2);
+    }
+}
+
+fn run(conf: Conf) -> Result<(), Box<Error>> {
     println!("Searching for '{}' in file '{}'",
              conf.query,
              conf.input_path);
-    let mut file = File::open(&conf.input_path).expect(format!("File '{}' not found",
-                                                               conf.input_path)
-                                                               .as_str());
+    let mut file = File::open(conf.input_path)?;
     let mut contents = String::new();
-    file.read_to_string(&mut contents)
-        .expect(format!("error reading file '{}'", conf.input_path).as_str());
-    println!("With contents\n{}", contents);
+    file.read_to_string(&mut contents)?;
+    println!("With text:\n{}", contents);
+    Ok(())
 }
 
 struct Conf {
