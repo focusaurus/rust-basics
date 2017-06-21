d496fb10d5ee5dde756a34697dc82273cbb1b41f
Peter Lyons <pete@peterlyons.com>
2017-06-21 10:17:37 -0600

    Define Conf struct and parse function

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -4,16 +4,30 @@ use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let conf = parse_args(&args);
     println!("{:?}", args);
-    let query = &args[1];
-    let target = &args[2];
-    println!("Searching for '{}' in file '{}'", query, target);
-    let mut file = File::open(target).expect(format!("File '{}' not found", target).as_str());
+    println!("Searching for '{}' in file '{}'",
+             conf.query,
+             conf.input_path);
+    let mut file = File::open(&conf.input_path).expect(format!("File '{}' not found",
+                                                               conf.input_path)
+                                                               .as_str());
     let mut contents = String::new();
     file.read_to_string(&mut contents)
-        .expect(format!("error reading file '{}'", target).as_str());
+        .expect(format!("error reading file '{}'", conf.input_path).as_str());
     println!("With contents\n{}", contents);
     // for arg in env::args() {
     //     println!("{:?}", arg);
     // }
+}
+
+struct Conf {
+    query: String,
+    input_path: String,
+}
+fn parse_args(args: &[String]) -> Conf {
+    Conf {
+        query: args[1].clone(),
+        input_path: args[2].clone(),
+    }
 }
