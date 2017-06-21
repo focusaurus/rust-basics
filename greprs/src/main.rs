3d37f4a3d0715c542ee811f01fafc1fdcf0e2219
Peter Lyons <pete@peterlyons.com>
2017-06-21 10:49:55 -0600

    greprs print usage

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -1,6 +1,8 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::Path;
+use std::ffi::OsStr;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -25,6 +27,27 @@ struct Conf {
 
 impl Conf {
     fn new(args: &[String]) -> Conf {
+        // let program = match Path::new(&args[0]).file_name().or(Some(OsStr::new("greprs")) {
+        //         None => OsStr::new("greprs"),
+        //         Some(osstr) => osstr,
+        //     }
+        //     .to_str()
+        //     .unwrap_or("greprs");
+        let program = Path::new(&args[0])
+            .file_name()
+            .unwrap_or(OsStr::new("greprs"))
+            .to_str()
+            .unwrap();
+        // if let Some(osstr) = Path::new(&args[0]).file_name() {
+        //     println!("osstr hey {:?}", osstr);
+        // } else {
+        //     println!("none");
+        // }
+
+        // .or(OsStr::new("greprs").to_str());
+        if args.len() < 3 {
+            panic!("Usage: {} <query> <input-path>", program);
+        }
         Conf {
             query: args[1].clone(),
             input_path: args[2].clone(),
