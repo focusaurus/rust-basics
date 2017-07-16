a0dd380bd5ac8fc30c6e069a9354131ad77e15de
Peter Lyons <pete@peterlyons.com>
2017-07-15 19:59:58 -0600

    Handle BrokenPipe error cleanly

--- hex_dump/src/main.rs
+++ hex_dump/src/main.rs
@@ -2,22 +2,33 @@ extern crate hex_dump;
 
 use std::io;
 use std::io::Read;
-// use std::io::Write;
+use std::io::Write;
 use std::io::ErrorKind;
 
 fn main() {
-    // let stdout = io::stdout();
+    use std::process;
+    let mut stdout = io::stdout();
+    let mut stderr = io::stderr();
     for item in io::stdin().bytes().enumerate() {
         match item {
             (index, Ok(byte)) => {
-                print!("{}", hex_dump::format_in_place(index, byte));
-            }
-            (_, Err(err)) => {
-                match err.kind() {
-                    ErrorKind::BrokenPipe => print!("hi"),
-                    _ => println!("{}", err),
+                let result = write!(stdout, "{}", hex_dump::format_in_place(index, byte));
+                match result {
+                    Ok(_) => (),
+                    Err(err) => {
+                        let code = match err.kind() {
+                            // This is OK, happens when piped to head
+                            ErrorKind::BrokenPipe => 0,
+                            _ => {
+                                writeln!(stderr, "{}", err).unwrap();
+                                1
+                            }
+                        };
+                        process::exit(code);
+                    }
                 }
             }
+            (_, Err(err)) => writeln!(stderr, "{}", err).unwrap(),
         }
     }
 }
