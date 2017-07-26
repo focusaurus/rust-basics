163b559bc16c3ed178d47230b03c54180a091a3b
Peter Lyons <pete@peterlyons.com>
2017-07-26 10:55:33 -0600

    Start Weak exercise

--- refcell/src/main.rs
+++ refcell/src/main.rs
@@ -18,4 +18,42 @@ fn main() {
     println!("Hello, world!");
     let value = RefCell::new(1);
     demo(&value);
+    let v2 = Rc::new(RefCell::new(5));
+    let a = Cons(v2.clone(), Rc::new(Nil));
+    let shared_list = Rc::new(a);
+    let b = Cons(Rc::new(RefCell::new(6)), shared_list.clone());
+    let c = Cons(Rc::new(RefCell::new(10)), shared_list.clone());
+
+    *v2.borrow_mut() += 10;
+    println!("shared_list after = {:?}", shared_list);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
+    main2();
+}
+
+// ------------------------
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+use std::rc::Rc;
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    children: RefCell<Vec<Rc<Node>>>,
+}
+
+fn main2() {
+    let leaf = Rc::new(Node {
+                           value: 3,
+                           children: RefCell::new(vec![]),
+                       });
+    let branch = Rc::new(Node {
+                             value: 5,
+                             children: RefCell::new(vec![leaf.clone()]),
+                         });
 }
