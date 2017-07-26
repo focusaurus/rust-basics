5fb39938ca4c3a8d93d5c2505a993610fc12c22d
Peter Lyons <pete@peterlyons.com>
2017-07-26 11:31:33 -0600

    join spawned thread

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,10 +1,11 @@
 use std::thread;
 
 fn main() {
-    thread::spawn(|| for i in 1..10 {
+    let handle = thread::spawn(|| for i in 1..10 {
                       println!("hi number {:?} from spawned thread", i);
                   });
     for i in 1..5 {
         println!("main {}", i);
     }
+    handle.join();
 }
