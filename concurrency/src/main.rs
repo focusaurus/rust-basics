4d909018c3e70b26b63d670912b99f51f8f7008e
Peter Lyons <pete@peterlyons.com>
2017-07-26 11:31:50 -0600

    wait for spawned

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -2,10 +2,10 @@ use std::thread;
 
 fn main() {
     let handle = thread::spawn(|| for i in 1..10 {
-                      println!("hi number {:?} from spawned thread", i);
-                  });
+                                   println!("hi number {:?} from spawned thread", i);
+                               });
+    handle.join();
     for i in 1..5 {
         println!("main {}", i);
     }
-    handle.join();
 }
