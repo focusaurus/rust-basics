277abd01c3bd4a0ae0c815927ef1b151dce57bd6
Peter Lyons <pete@peterlyons.com>
2017-07-26 11:49:50 -0600

    thread and sleep

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,11 +1,38 @@
 use std::thread;
 
-fn main() {
-    let handle = thread::spawn(|| for i in 1..10 {
-                                   println!("hi number {:?} from spawned thread", i);
+fn do_thread() {
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+                                   println!("vec from thread {:?}", v);
                                });
     handle.join();
     for i in 1..5 {
         println!("main {}", i);
     }
+}
+use std::sync::mpsc;
+use std::time::Duration;
+
+fn do_channel() {
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![String::from("hi"),
+                        String::from("from"),
+                        String::from("the"),
+                        String::from("thread")];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    for received in rx {
+        println!("Got: {:?}", received);
+
+    }
+}
+fn main() {
+    do_thread();
+    do_channel();
 }
