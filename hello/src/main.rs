90c1a2ccd4b90fbdce1a03ba68c858e2eedf9823
Peter Lyons <pete@peterlyons.com>
2017-10-04 09:21:24 -0600

    hello TCP basic listener

---
+++ hello/src/main.rs
@@ -0,0 +1,9 @@
+use std::net::TcpListener;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Client connected");
+    }
+}
