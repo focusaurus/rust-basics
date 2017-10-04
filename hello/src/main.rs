5082bfcef5ecc25c100708cf5e0693ba9d102938
Peter Lyons <pete@peterlyons.com>
2017-10-04 11:12:02 -0600

    hello response template

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -2,22 +2,27 @@ use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 use std::fs::File;
 
-static LINE: &'static str = "\r\n\r\n";
-static HTTP: &'static str = "HTTP/1.1";
+static LINE: &'static str = "\r\n";
+// static HTTP: &'static str = "HTTP/1.1";
 static GET: &'static [u8] = b"GET / HTTP/";
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
-    let mut contents = String::new();
+    let mut body = String::new();
     let (status_code, status_text, file_name) = if buffer.starts_with(GET) {
         (200, "OK", "index.html")
     } else {
         (400, "Not Found", "404.html")
     };
     let mut file = File::open(file_name).unwrap();
-    file.read_to_string(&mut contents).unwrap();
-    let response = format!("{} {} {} {}{}", HTTP, status_code, status_text, LINE, contents);
+    file.read_to_string(&mut body).unwrap();
+    let response = format!("HTTP/1.1 {status_code} {status_text}{l}Content-Length: {content_length}{l}{l}{body}",
+                           status_code = status_code,
+                           status_text = status_text,
+                           l = LINE,
+                           content_length = body.len(),
+                           body = body);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
