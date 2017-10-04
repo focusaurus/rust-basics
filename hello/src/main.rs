29638fc6b6a79634346d86a663eeb0ac178abaae
Peter Lyons <pete@peterlyons.com>
2017-10-04 10:59:03 -0600

    hello clean up response formatting

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -3,20 +3,21 @@ use std::net::{TcpListener, TcpStream};
 use std::fs::File;
 
 static LINE: &'static str = "\r\n\r\n";
+static HTTP: &'static str = "HTTP/1.1";
 static GET: &'static [u8] = b"GET / HTTP/";
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
     let mut contents = String::new();
-    let (status, file_name) = if buffer.starts_with(GET) {
-        ("HTTP/1.1 200 OK", "index.html")
+    let (status_code, status_text, file_name) = if buffer.starts_with(GET) {
+        (200, "OK", "index.html")
     } else {
-        ("HTTP/1.1 400 Not Found", "404.html")
+        (400, "Not Found", "404.html")
     };
     let mut file = File::open(file_name).unwrap();
     file.read_to_string(&mut contents).unwrap();
-    let response = format!("{}{}{}", status, LINE, contents);
+    let response = format!("{} {} {} {}{}", HTTP, status_code, status_text, LINE, contents);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
