bde1492b09f7686f1521d6386589fdcc1e29bfa1
Peter Lyons <pete@peterlyons.com>
2017-10-04 10:04:45 -0600

    hello web server prints first chunk of request

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,9 +1,17 @@
-use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::{TcpListener,TcpStream};
+// use std::net::TcpListener;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
         println!("Client connected");
+        handle_connection(stream.unwrap());
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
