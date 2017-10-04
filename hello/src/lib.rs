a6ea2ea4d77f3cbfe267b4d16c1f3282f51caf7c
Peter Lyons <pete@peterlyons.com>
2017-10-04 12:19:54 -0600

    hello got thread pool working

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,23 +1,38 @@
 use std::thread;
+use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
 
+trait FnBox {
+    fn call_box(self: Box<Self>);
+}
+
+impl<F: FnOnce()> FnBox for F {
+    fn call_box(self: Box<F>) {
+        (*self)()
+    }
+}
 struct Worker {
     id: usize,
-    handle: thread::JoinHandle<()>,
+    thread: thread::JoinHandle<()>,
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         Worker {
             id,
-            handle: thread::spawn(|| {}),
+            thread: thread::spawn(move || loop {
+                                      let job = receiver.lock().unwrap().recv().unwrap();
+                                      println!("Worker {:?} executing job", id);
+                                      job.call_box();
+                                  }),
         }
     }
 }
 
-// struct Job;
-
+type Job = Box<FnBox + Send + 'static>;
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
 impl ThreadPool {
@@ -28,11 +43,18 @@ impl ThreadPool {
     /// The `new` function will panic if size is zero.
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
         let mut workers = Vec::with_capacity(size);
-        for i in 0..size {
-            workers.push(Worker::new(i));
+        for id in 0..size {
+            workers.push(Worker::new(id, receiver.clone()));
         }
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
-    pub fn execute<F>(&self, f: F) where F: FnOnce() + Send + 'static {}
+    pub fn execute<F>(&self, f: F)
+        where F: FnOnce() + Send + 'static
+    {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
 }
