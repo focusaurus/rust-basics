6fb9aa5ba85a0e6cdc3cfd71074081daaa572489
Peter Lyons <pete@peterlyons.com>
2017-10-04 10:21:35 -0600

    hello basic HTTP response

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -13,5 +13,8 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
