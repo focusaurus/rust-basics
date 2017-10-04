bcd5e1e6ade53ef70fe36fb31ae277b0b56bd4ef
Peter Lyons <pete@peterlyons.com>
2017-10-04 10:49:18 -0600

    hello add support for 404 Not Found page

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,6 +1,27 @@
 use std::io::prelude::*;
-use std::net::{TcpListener,TcpStream};
+use std::net::{TcpListener, TcpStream};
 use std::fs::File;
+
+static LINE: &'static str = "\r\n\r\n";
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    let get = b"GET / HTTP/";
+    let mut contents = String::new();
+    let response;
+    if buffer.starts_with(get) {
+        let mut file = File::open("index.html").unwrap();
+        file.read_to_string(&mut contents).unwrap();
+        response = format!("HTTP/1.1 200 OK{}{}", LINE, contents);
+    } else {
+        let mut file = File::open("404.html").unwrap();
+        file.read_to_string(&mut contents).unwrap();
+        response = format!("HTTP/1.1 404 Not Found{}{}", LINE, contents);
+    }
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
@@ -8,15 +29,4 @@ fn main() {
         println!("Client connected");
         handle_connection(stream.unwrap());
     }
-}
-
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
-    let mut file = File::open("index.html").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    let response =format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
 }
