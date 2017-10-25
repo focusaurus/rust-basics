052823eee33eed949ad3d190494920836a5ebbb6
Peter Lyons <pete@peterlyons.com>
2017-10-25 10:25:05 -0600

    Rust by example ch 3 first pass

--- rust-by-example/src/main.rs
+++ rust-by-example/src/main.rs
@@ -1,66 +1,75 @@
-use std::fmt::Formatter;
-use std::fmt;
-use std::fmt::Error;
-
-// Tuples can be used as function arguments and as return values
-fn reverse(pair: (i32, bool)) -> (bool, i32) {
-    // `let` can be used to bind the members of a tuple to variables
-    let (integer, boolean) = pair;
-
-    (boolean, integer)
-}
-
-// The following struct is for the activity.
 #[derive(Debug)]
-struct Matrix(f32, f32, f32, f32);
-
-impl fmt::Display for Matrix {
-    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        write!(f, "( {}, {} )\n( {} {} )", self.0, self.1, self.2, self.3)
-    }
+struct Person<'a> {
+    name: &'a str,
+    age: u8,
 }
 
-fn transpose(matrix: Matrix) -> Matrix {
-    Matrix(matrix.0, matrix.2, matrix.1, matrix.3)
+// A unit struct
+struct Nil;
+
+// A tuple struct
+struct Pair(i32, f32);
+
+// A struct with two fields
+#[derive(Debug)]
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+// Structs can be reused as fields of another struct
+#[allow(dead_code)]
+#[derive(Debug)]
+
+struct Rectangle {
+    p1: Point,
+    p2: Point,
+}
+
+fn rect_area(rect: Rectangle) -> f32 {
+    let width = rect.p1.x - rect.p2.x;
+    let height = rect.p1.y - rect.p2.y;
+    width.abs() * height.abs()
 }
 
 fn main() {
-    // A tuple with a bunch of different types
-    let long_tuple = (1u8, 2u16, 3u32, 4u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, 'a', true);
+    // Create struct with field init shorthand
+    let name = "Peter";
+    let age = 27;
+    let peter = Person { name, age };
 
-    // Values can be extracted from the tuple using tuple indexing
-    println!("long tuple first value: {}", long_tuple.0);
-    println!("long tuple second value: {}", long_tuple.1);
+    // Print debug struct
+    println!("{:?}", peter);
 
-    // Tuples can be tuple members
-    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
 
-    // Tuples are printable
-    println!("tuple of tuples: {:?}", tuple_of_tuples);
+    // Instantiate a `Point`
+    let point: Point = Point { x: 0.3, y: 0.4 };
 
-    // But long Tuples cannot be printed
-    // let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
-    // println!("too long tuple: {:?}", too_long_tuple);
-    // TODO ^ Uncomment the above 2 lines to see the compiler error
+    // Access the fields of the point
+    println!("point coordinates: ({}, {})", point.x, point.y);
 
-    let pair = (1, true);
-    println!("pair is {:?}", pair);
+    // Destructure the point using a `let` binding
+    let Point { x: my_x, y: my_y } = point;
 
-    println!("the reversed pair is {:?}", reverse(pair));
+    let _rectangle = Rectangle {
+        // struct instantiation is an expression too
+        p1: Point { x: 0.0, y: 0.0 },
+        p2: Point { x: 4.0, y: 2.0},
+    };
 
-    // To create one element tuples, the comma is required to tell them apart
-    // from a literal surrounded by parentheses
-    println!("one element tuple: {:?}", (5u32,));
-    println!("just an integer: {:?}", (5u32));
+    // Instantiate a unit struct
+    let _nil = Nil;
 
-    //tuples can be destructured to create bindings
-    let tuple = (1, "hello", 4.5, true);
+    // Instantiate a tuple struct
+    let pair = Pair(1, 0.1);
 
-    let (a, b, c, d) = tuple;
-    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+    // Access the fields of a tuple struct
+    println!("pair contains {:?} and {:?}", pair.0, pair.1);
 
-    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
-    println!("{}", matrix);
-    println!("{}", transpose(matrix));
+    // Destructure a tuple struct
+    let Pair(integer, decimal) = pair;
 
+    println!("pair contains {:?} and {:?}", integer, decimal);
+    println!("rectangle {:?}", _rectangle);
+    println!("area: {}", rect_area(_rectangle));
 }
