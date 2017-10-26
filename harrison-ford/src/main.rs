cd74af086d4090aa295fe19341d045e477106790
Peter Lyons <pete@peterlyons.com>
2017-10-26 13:19:27 -0600

    BTreeSet

--- harrison-ford/src/main.rs
+++ harrison-ford/src/main.rs
@@ -1,10 +1,8 @@
+use std::collections::BTreeSet;
+
+const ACTIONS: &str = "Mustage Escape Escort Falcon Fiesta Focus";
 fn main() {
-    let mut actions = "Mustage Escape Escort Falcon Fiesta Focus"
-        .split_whitespace()
-        .collect::<Vec<_>>();
-    // let mut actions = vec!["Mustang", "Escape", "Escort", "Falcon", "Fiesta", "Focus"];
-    actions.sort();
-    for action in actions {
+    for action in ACTIONS.split_whitespace().collect::<BTreeSet<_>>() {
         println!("Ford {}", action);
     }
 }
