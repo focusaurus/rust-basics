5b553d83639854b797b8024df264b0c87cfde06c
Peter Lyons <pete@peterlyons.com>
2017-10-26 13:32:39 -0600

    Sorted trait

--- harrison-ford/src/main.rs
+++ harrison-ford/src/main.rs
@@ -1,8 +1,30 @@
-use std::collections::BTreeSet;
 
+trait Sorted {
+    fn sorted(mut self) -> Self;
+}
+
+impl<T> Sorted for Vec<T>
+    where T: Ord
+{
+    fn sorted(mut self) -> Self {
+        self.sort();
+        self
+    }
+}
+const ACTIONS: &str = "Mustage Escape Escort Falcon Fiesta Focus";
+
+fn main() {
+    for action in ACTIONS.split_whitespace().collect::<Vec<_>>().sorted(){
+        println!("Ford {}", action);
+    }
+}
+
+/*
+use std::collections::BTreeSet;
 const ACTIONS: &str = "Mustage Escape Escort Falcon Fiesta Focus";
 fn main() {
     for action in ACTIONS.split_whitespace().collect::<BTreeSet<_>>() {
         println!("Ford {}", action);
     }
 }
+*/
