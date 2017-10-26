da3bde656ff4f4c9e382eb61134be28f1e76c7bb
Peter Lyons <pete@peterlyons.com>
2017-10-26 14:10:08 -0600

    fizzbuzz match

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,15 +1,27 @@
+// fn main() {
+//     for number in 1..101 {
+//         let mut message = String::new();
+//         if number % 3 == 0 {
+//             message.push_str("Fizz");
+//         }
+//         if number % 5 == 0 {
+//             message.push_str("Buzz");
+//         }
+//         if message.len() == 0 {
+//             message.push_str(format!("{}", number).as_str());
+//         }
+//         println!("{}", message);
+//     }
+// }
 fn main() {
+
     for number in 1..101 {
-        let mut message = String::new();
-        if number % 3 == 0 {
-            message.push_str("Fizz");
-        }
-        if number % 5 == 0 {
-            message.push_str("Buzz");
-        }
-        if message.len() == 0 {
-            message.push_str(format!("{}", number).as_str());
-        }
+        let message = match (number % 3, number % 5) {
+            (0, 0) => "FizzBuzz".to_string(),
+            (0, _) => "Fizz".to_string(),
+            (_, 0) => "Buzz".to_string(),
+            _ => format!("{}", number),
+        };
         println!("{}", message);
     }
 }
