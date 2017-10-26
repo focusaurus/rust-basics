5a2d95db75bab1fc8bb90a722a6af34e90bb39e1
Peter Lyons <pete@peterlyons.com>
2017-10-26 13:09:46 -0600

    turbofish

--- harrison-ford/src/main.rs
+++ harrison-ford/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
-    let actions = ["Escape", "Escort", "Falcon", "Fiesta", "Focus", "Mustang"];
-    for action in actions.iter() {
+    let mut actions = "Mustage Escape Escort Falcon Fiesta Focus"
+        .split_whitespace()
+        .collect::<Vec<_>>();
+    // let mut actions = vec!["Mustang", "Escape", "Escort", "Falcon", "Fiesta", "Focus"];
+    actions.sort();
+    for action in actions {
         println!("Ford {}", action);
     }
 }
