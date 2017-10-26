49800da523b21d478e9fe0bf2d52121b4e5ddd17
Peter Lyons <pete@peterlyons.com>
2017-10-26 12:24:09 -0600

    Harrison Ford hello world

---
+++ harrison-ford/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Ford Escape");
+    println!("Ford Escort");
+    println!("Ford Falcon");
+    println!("Ford Fiesta");
+    println!("Ford Focus");
+    println!("Ford Mustang");
+}
