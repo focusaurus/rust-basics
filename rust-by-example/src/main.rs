66229a5b94aed52ffd3dc442afe8b2c03bac6c4f
Peter Lyons <pete@peterlyons.com>
2017-11-03 08:43:36 -0600

    rust by example type alias

--- rust-by-example/src/main.rs
+++ rust-by-example/src/main.rs
@@ -1,47 +1,21 @@
-// Suppress all warnings from casts which overflow.
+// `NanoSecond` is a new name for `u64`.
+type NanoSecond = u64;
+type Inch = u64;
+
+// Use an attribute to silence warning.
+// #[allow(non_camel_case_types)]
+type Hey = u64;
+// TODO ^ Try removing the attribute
 
 fn main() {
-    let decimal = 65.4321_f32;
+    // `NanoSecond` = `Inch` = `Hey` = `u64`.
+    let nanoseconds: NanoSecond = 5 as Hey;
+    let inches: Inch = 2 as Hey;
 
-    // Error! No implicit conversion
-     let integer: u8 = decimal as u8;
-    // FIXME ^ Comment out this line
-
-    // Explicit conversion
-    let integer = decimal as u8;
-    let character = integer as char;
-
-    println!("Casting: {} -> {} -> {}", decimal, integer, character);
-
-    // when casting any value to an unsigned type, T,
-    // std::T::MAX + 1 is added or subtracted until the value
-    // fits into the new type
-
-    // 1000 already fits in a u16
-    println!("1000 as a u16 is: {}", 1000 as u16);
-
-    // 1000 - 256 - 256 - 256 = 232
-    // Under the hood, the first 8 least significant bits (LSB) are kept,
-    // while the rest towards the most significant bit (MSB) get truncated.
-    println!("1000 as a u8 is : {}", 1000 as u8);
-    // -1 + 256 = 255
-    println!("  -1 as a u8 is : {}", (-1i8) as u8);
-
-    // For positive numbers, this is the same as the modulus
-    println!("1000 mod 256 is : {}", 1000 % 256);
-
-    // When casting to a signed type, the (bitwise) result is the same as
-    // first casting to the corresponding unsigned type. If the most significant
-    // bit of that value is 1, then the value is negative.
-
-    // Unless it already fits, of course.
-    println!(" 128 as a i16 is: {}", 128 as i16);
-    // 128 as u8 -> 128, whose two's complement in eight bits is:
-    println!(" 128 as a i8 is : {}", 128 as i8);
-
-    // repeating the example above
-    // 1000 as u8 -> 232
-    println!("1000 as a u8 is : {}", 1000 as u8);
-    // and the two's complement of 232 is -24
-    println!(" 232 as a i8 is : {}", 232 as i8);
+    // Note that type aliases *don't* provide any extra type safety, because
+    // aliases are *not* new types
+    println!("{} nanoseconds + {} inches = {} unit?",
+             nanoseconds,
+             inches,
+             nanoseconds + inches);
 }
