15b7449d5e647349784019bb8d41720d1f7040f4
Peter Lyons <pete@peterlyons.com>
2017-11-06 06:19:11 -0700

    Fn generic experiment

--- rust-by-example/src/main.rs
+++ rust-by-example/src/main.rs
@@ -4,8 +4,20 @@ fn once<F>(to_call: F) -> bool
     to_call() > 10
 }
 
-fn mutable<F>(mut to_call: F) -> bool where F: FnMut(i32) -> i32 {
+fn mutable<F>(mut to_call: F) -> bool
+    where F: FnMut(i32) -> i32
+{
     to_call(2) > 10
+}
+
+fn by_ref<F>(to_call: F) -> bool
+    where F: Fn(&i32) -> i32
+{
+    to_call(&2) > 10
+}
+
+fn by_ref2(input: &i32) -> i32 {
+    input * 3
 }
 
 fn main() {
@@ -15,4 +27,10 @@ fn main() {
     // to_call_once();
     let to_call_mut = |value| value * 2;
     println!("mutable: {:?}", mutable(to_call_mut));
+
+    let to_call_ref = |value: &i32| value * 3;
+    println!("by_ref: {:?}", by_ref(by_ref2));
+    println!("by_ref: {:?}", by_ref(to_call_ref));
+    println!("by_ref2: {:?}", by_ref2(&23));
+
 }
