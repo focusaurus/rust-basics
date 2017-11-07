f41ace9e2b5201d55d6dac3f30901703164ef586
Peter Lyons <pete@peterlyons.com>
2017-11-07 12:23:59 -0700

    WIP on shuffle

--- tirefox/src/main.rs
+++ tirefox/src/main.rs
@@ -1,19 +1,34 @@
+// todo generate a pair of random integers
+// todo try shuffled_iter
+extern crate shuffled_iter;
+mod scratch;
+
+// use random::Source;
 use std::fs;
 use std::io;
 use std::io::prelude::*;
 
 fn main() {
+    scratch::scratch();
+    // let mut source = random::default().seed([48654684, 7686898]);
+    // let mut index = source.read::<usize>();
     let words_file = fs::File::open("/usr/share/dict/words").unwrap();
     let words_reader = io::BufReader::new(words_file);
-    let mut count = 0;
-
-    for word in words_reader
-            .lines()
-            .map(|result| result.unwrap())
-            .filter(|line| line.len() < 8) {
-        count += 1;
-        println!("{}", word);
-    }
-    println!("words: {}", count);
+    // let word = words_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .filter(|line| line.len() < 8)
+    //     .nth(index).unwrap();
+    // println!("{}", word);
+    // let mut count = 0;
+    //
+    // for word in words_reader
+    //         .lines()
+    //         .map(|result| result.unwrap())
+    //         .filter(|line| line.len() < 8) {
+    //     count += 1;
+    //     println!("{}", word);
+    // }
+    // println!("words: {}", count);
 }
 // todo use .nth() to get some random ones
