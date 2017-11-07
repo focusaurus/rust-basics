28b5d5d0768064440d27e418ec7871946c6471b4
Peter Lyons <pete@peterlyons.com>
2017-11-07 13:16:48 -0700

    WIP shuffle

--- tirefox/src/scratch.rs
+++ tirefox/src/scratch.rs
@@ -1,6 +1,8 @@
 // todo get a shuffled iter going with RangeTo etc
 extern crate rand;
+use shuffled_iter;
 use shuffled_iter::ShuffledIterGen;
+
 use std::iter;
 // use rand::Rng;
 
@@ -18,10 +20,11 @@ pub fn scratch() {
         println!("{:?}", x);
     }
 
-
-        for x in rng.iter_shuffled(0..6) {
-            println!("{:?}", x);
-        }
+    // let mut iter: shuffled_iter::ShuffledIter = rng.iter_shuffled(0..6).take::<u32>(3u32);
+    let mut iter: shuffled_iter::ShuffledIter<Item = u32> = rng.iter_shuffled(0..6).take(3);
+    for x in iter {
+        println!("{:?}", x);
+    }
 
     /* won't compile
     let words = &["a", "b", "c"];
