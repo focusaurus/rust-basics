bb08a49d06baf353c439b3dd74dd904019357a6c
Peter Lyons <pete@peterlyons.com>
2017-11-07 15:42:36 -0700

    WIP on tirefox have random pairs of indices

--- tirefox/src/main.rs
+++ tirefox/src/main.rs
@@ -1,7 +1,9 @@
 // todo generate a pair of random integers
 // todo try shuffled_iter
+extern crate rand;
 extern crate shuffled_iter;
 mod scratch;
+use shuffled_iter::ShuffledIterGen;
 
 // use random::Source;
 use std::fs;
@@ -9,16 +11,39 @@ use std::io;
 use std::io::prelude::*;
 
 fn main() {
-    scratch::scratch();
+    // scratch::scratch();
     // let mut source = random::default().seed([48654684, 7686898]);
     // let mut index = source.read::<usize>();
     let words_file = fs::File::open("/usr/share/dict/words").unwrap();
     let words_reader = io::BufReader::new(words_file);
+    let how_many = 4;
+    let short_word_count= words_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .filter(|line| line.len() < 7)
+        .count();
+    println!("{:?}", short_word_count);
+    let mut chaos = rand::thread_rng();
+    // let mut pair: [Option<u32>; 2] = [None, None];
+    let mut first: Option<u32> = None;
+    for index in chaos.iter_shuffled(0u32..short_word_count as u32).take(how_many*2) {
+        if first.is_none() {
+            first = Some(index);
+        } else {
+            println!("indices: {}, {}", first.unwrap(), index);
+            first = None;
+        }
+    }
+        //
+    // ✓ get length of filtered word iter
+    // get a shuffle rng with range 0 to wordLength
+    // take N out of that to use as indexes where N <= shortWords.len()
+    //
     // let word = words_reader
     //     .lines()
     //     .map(|result| result.unwrap())
     //     .filter(|line| line.len() < 8)
-    //     .nth(index).unwrap();
+    //     .count(index).unwrap();
     // println!("{}", word);
     // let mut count = 0;
     //
--- tirefox/src/scratch.rs
+++ tirefox/src/scratch.rs
@@ -15,16 +15,23 @@ pub fn scratch() {
         println!("{:?}", word);
     }
 
-    let mut rng = rand::XorShiftRng::new_unseeded();
-    for x in rng.iter_shuffled(0..6) {
+    // let mut rng = rand::XorShiftRng::new_unseeded();
+    // for x in rng.iter_shuffled(0..6) {
+    //     println!("{:?}", x);
+    // }
+
+    let mut rng = rand::thread_rng();
+    for x in rng.iter_shuffled(0u32..6u32).take(3) {
+        // Type errors: try innermost thing annotating first
         println!("{:?}", x);
     }
-
     // let mut iter: shuffled_iter::ShuffledIter = rng.iter_shuffled(0..6).take::<u32>(3u32);
+    /*
     let mut iter: shuffled_iter::ShuffledIter<Item = u32> = rng.iter_shuffled(0..6).take(3);
     for x in iter {
         println!("{:?}", x);
     }
+*/
 
     /* won't compile
     let words = &["a", "b", "c"];
