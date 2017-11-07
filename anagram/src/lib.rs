03c286b3955fc30c318d6b22d68fe39e6faec3c3
Peter Lyons <pete@peterlyons.com>
2017-11-06 18:54:28 -0700

    anagram WIP

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,3 +1,30 @@
+use std::iter::FromIterator;
+
+fn normalize(word: String) -> String {
+    let mut lower_letters: Vec<char> = word.to_lowercase().chars().collect();
+    lower_letters.sort();
+    String::from_iter(lower_letters)
+}
+// fn normalize<'a>(word: &str) -> &str {
+//     let mut lower_letters: &'a Vec<char> = word.to_lowercase().chars().collect();
+//     lower_letters.sort();
+//     String::from_iter(lower_letters).as_str()
+// }
+// fn normalize<'a>(word: &'a str) -> &'a str {
+//     let mut lower_letters: Vec<char> = word.to_lowercase().chars().collect();
+//     lower_letters.sort();
+//     String::from_iter(lower_letters).as_str()
+// }
+
 pub fn anagrams_for<'a>(base_word: &str, words: &[&str]) -> Vec<&'a str> {
+    let normal_base = normalize(String::from(base_word));
+    // words
+    //     .iter()
+    //     .filter(|&&word| normalize(String::from(word)) == normal_base)
+    //     .collect::<Vec<&str>>()
+    // let hey = words.into_iter().filter(|&word| word.len() > 0).collect::<Vec<&&str>>();
+    let hey = words.into_iter().filter(|word| word.len() > 0).collect::<Vec<&&str>>();
+    println!("HEY {:?}", hey);
     vec![]
+    // hey
 }
