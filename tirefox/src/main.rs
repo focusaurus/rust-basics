10f98c624053adc01ed916ca4ece346395fec608
Peter Lyons <pete@peterlyons.com>
2017-11-08 09:38:41 -0700

    tirefox better stderr writing

--- tirefox/src/main.rs
+++ tirefox/src/main.rs
@@ -1,51 +1,37 @@
+
 // todo generate a pair of random integers
 // todo try shuffled_iter
 extern crate rand;
 extern crate shuffled_iter;
 mod scratch;
 use shuffled_iter::ShuffledIterGen;
-
+use std::error::Error;
 use std::fs;
 use std::io;
-use std::io::prelude::*;
 use std::io::ErrorKind;
+use std::io::prelude::*;
 use std::io::Write;
-use std::error::Error;
 
 const HOW_MANY: usize = 50;
 
+fn bail(err: std::io::Error) {
+    let message = match err.kind() {
+        ErrorKind::NotFound => "Words dictionary file not found",
+        _ => err.description(),
+    };
+    write!(&mut io::stderr(), "{}\n", message);
+    std::process::exit(10);
+}
+
 fn main() {
     // scratch::scratch();
-    let words_file = fs::File::open("/usr/share/dict/wordsNOPE")
-        .map_err(|err| {
-            let message = match err.kind() {
-                ErrorKind::NotFound => "Words dictionary file not found",
-                _ => err.description(),
-            };
-            io::stderr().write_fmt(format_args!("{}\n", message));
-            std::process::exit(10);
-            // if let Some(os_err) = err.raw_os_error() {
-            //     let message = match os_err {
-            //         2 => "Words dictionary file not found",
-            //         _ => err.description(),
-            //     };
-            //     println!("Error {:?}", err);
-            //     std::process::exit(10);
-            //
-            //
-            //     println!("raw OS error: {:?}", raw_os_err);
-            // } else {
-            //     println!("Not an OS error");
-            // }
-        })
+    let words_file = fs::File::open("/usr/share/dict/words")
+        .map_err(bail)
         .unwrap();
-    // if words_result.is_err() {
-    // }
-    // if let Err(err) = words_result {
     let words_reader = io::BufReader::new(words_file);
     let short_word_count = words_reader
         .lines()
-        .map(|result| result.unwrap())
+        .map(|result| result.map_err(bail).unwrap())
         .filter(|line| line.len() < 7)
         .count();
     // println!("{:?}", short_word_count);
@@ -61,15 +47,15 @@ fn main() {
     // words_file.seek(io::SeekFrom::Start(0));
     let mut short_word_iter = io::BufReader::new(&words_file2)
         .lines()
-        .map(|result| result.unwrap())
+        .map(|result| result.map_err(bail).unwrap())
         .filter(|line| line.len() < 7);
     let mut last = 0;
     let results = &indices
                        .iter()
                        .map(|&index| {
-                                let word = short_word_iter.nth(index - last - 1);
+                                let word = short_word_iter.nth(index - last - 1).unwrap();
                                 last = index;
-                                word.unwrap()
+                                word
                             })
                        .collect::<Vec<_>>();
     for pair in results.chunks(2) {
