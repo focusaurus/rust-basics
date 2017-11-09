825e390aac09a5f5bf204bf526879852e48665d5
Peter Lyons <pete@peterlyons.com>
2017-11-09 07:45:56 -0700

    scratch iterator chain stackoverflow question

--- scratch/src/main.rs
+++ scratch/src/main.rs
@@ -1,21 +1,58 @@
 use std::fs;
 use std::io;
 use std::io::prelude::*;
+use std::iter;
 
-fn main() {
-    let file = fs::File::open("/usr/share/dict/words").unwrap();
+const WORDS_PATH: &str = "/usr/share/dict/words";
+
+fn is_short(word: &String) -> bool {
+    word.len() < 7
+}
+
+fn unwrap(result: Result<String, io::Error>) -> String {
+    result.unwrap()
+}
+
+fn main_works_but_code_dupe() {
+    let file = fs::File::open(WORDS_PATH).unwrap();
     let reader = io::BufReader::new(&file);
-    let count = reader.lines().count();
+    let count = reader.lines().map(unwrap).filter(is_short).count();
     println!("{:?}", count);
 
-    // let file = fs::File::open("/usr/share/dict/words").unwrap();
     let mut reader = io::BufReader::new(&file);
     reader.seek(io::SeekFrom::Start(0));
-    for line in reader
-            .lines()
-            .map(|line| line.unwrap())
-            .filter(|line| line.len() > 7) {
-        println!("hey {:?}", line);
+    let sample_size = (0.05 * count as f32) as usize; // 5% sample
 
+    // This chain of iterator logic is duplicated
+    for line in reader.lines().map(unwrap).filter(is_short).take(sample_size) {
+        println!("{}", line);
     }
+}
+
+fn short_lines<'a, T>
+    (reader: &'a T)
+     -> iter::Filter<std::iter::Map<std::io::Lines<T>, &FnMut(&str, bool)>, &FnMut(&str, bool)>
+    where T: io::BufRead
+{
+    reader.lines().map(unwrap).filter(is_short)
+}
+
+fn main_dry() {
+    let file = fs::File::open(WORDS_PATH).unwrap();
+    let reader = io::BufReader::new(&file);
+    let count = short_lines(reader).count();
+    println!("{:?}", count);
+
+    // Would like to do this instead:
+    let mut reader = io::BufReader::new(&file);
+    reader.seek(io::SeekFrom::Start(0));
+    let sample_size = (0.05 * count as f32) as usize; // 5% sample
+    for line in short_lines(reader).take(sample_size) {
+        println!("{}", line);
+    }
+}
+
+
+fn main() {
+    main_works_but_code_dupe();
 }
--- tirefox/src/main.rs
+++ tirefox/src/main.rs
@@ -9,13 +9,14 @@ use std::io::prelude::*;
 use std::io::Write;
 
 const HOW_MANY: usize = 50;
+const WORDS_PATH: &str = "/usr/share/dict/words";
 
 fn bail(err: std::io::Error) {
     let message = match err.kind() {
         ErrorKind::NotFound => "Words dictionary file not found",
         _ => err.description(),
     };
-    write!(&mut io::stderr(), "{}\n", message);
+    write!(&mut io::stderr(), "{}\n", message).unwrap();
     std::process::exit(10);
 }
 
@@ -27,40 +28,19 @@ fn bail_unwrap(result: Result<String, io::Error>) -> String {
     result.map_err(bail).unwrap()
 }
 
-fn sorted_indices(max: usize) -> Vec<usize> {
+fn sorted_indices(max: usize, how_many: Option<usize>) -> Vec<usize> {
     let mut chaos = rand::thread_rng();
     let mut indices: Vec<usize> = chaos
         .iter_shuffled(0usize..max as usize)
-        .take(HOW_MANY * 2)
+        .take(how_many.unwrap_or(50) * 2)
         .collect();
     indices.sort();
     indices
 }
 
-// fn index_pairs<'a>(indices: &'a Vec<usize>, iter: &mut Iterator<Item=&'a str>) -> &'a Vec<&'a str> {
-//         let mut last = 0;
-//         let results = &indices
-//                            .iter()
-//                            .map(|&index| {
-//                                     let word = iter.nth(index - last - 1).unwrap();
-//                                     last = index;
-//                                     word
-//                                 })
-//                            .collect::<Vec<&'a str>>();
-// results
-// }
-//
-// fn iter_short_lines(reader: &io::BufReader<fs::File>) -> &std::iter::Filter<> {
-//             reader.lines()
-//             .map(bail_unwrap)
-//             .filter(is_short)
-// }
-
 fn main() {
     // count total words (one per line) in the file
-    let words_file = fs::File::open("/usr/share/dict/words")
-        .map_err(bail)
-        .unwrap();
+    let words_file = fs::File::open(WORDS_PATH).map_err(bail).unwrap();
     let words_reader = io::BufReader::new(&words_file);
     let short_word_count = words_reader
         .lines()
@@ -70,13 +50,10 @@ fn main() {
 
     // Prepare the iterator of the words themselves
     let mut reader = io::BufReader::new(&words_file);
-    reader.seek(io::SeekFrom::Start(0));
-    let mut short_word_iter = reader
-        .lines()
-        .map(bail_unwrap)
-        .filter(is_short);
+    reader.seek(io::SeekFrom::Start(0)).unwrap();
+    let mut short_word_iter = reader.lines().map(bail_unwrap).filter(is_short);
 
-    let mut indices = sorted_indices(short_word_count);
+    let indices = sorted_indices(short_word_count, Some(HOW_MANY));
     let mut last = 0;
     let results = &indices
                        .iter()
