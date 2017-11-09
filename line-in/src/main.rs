e404043b1e55ac1cc28ec5e04c499cedf373b11c
Peter Lyons <pete@peterlyons.com>
2017-11-09 13:34:05 -0700

    line-in refactor. Getting bad file descriptor error.

--- line-in/src/main.rs
+++ line-in/src/main.rs
@@ -20,6 +20,13 @@ fn bail(err: std::io::Error) {
     error_exit(String::from(message), 10);
 }
 
+fn has_line(target_path: &str, to_add: &str) -> bool {
+    let target_file = fs::File::open(target_path).map_err(bail).unwrap();
+    io::BufReader::new(&target_file)
+        .lines()
+        .any(|line| line.unwrap().as_str() == to_add)
+}
+
 fn main() {
     println!("main4");
     let args = env::args().collect::<Vec<String>>();
@@ -31,21 +38,16 @@ fn main() {
             .to_str()
             .unwrap();
 
-        error_exit(format!("Usage: {} <target_file> <line_to_add>\n", program_name),
-                   1);
+        error_exit(format!("Usage: {} <target_file> <line_to_add>\n", program_name), 1);
     }
     let target_path = &args[1];
     let to_add = &args[2];
-    let mut target_file = fs::File::open(target_path).map_err(bail).unwrap();
-    let reader = io::BufReader::new(&target_file);
-    let found = reader
-        .lines()
-        .any(|line| line.unwrap().as_str() == to_add);
-    if !found {
+    if !has_line(target_path, to_add) {
         println!("need to add");
-        // target_file.seek(SeekFrom::End(0));
-        // target_file.write(&to_add.into_bytes());
+        let mut target_file = fs::File::open(target_path).map_err(bail).unwrap();
+        target_file.seek(SeekFrom::End(0));
+        target_file.write(&to_add.clone().into_bytes()).unwrap();
     } else {
-        println!("alread there");
+        println!("line is already there");
     }
 }
