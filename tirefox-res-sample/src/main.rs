4de8712010d30afac66af3bbf163f0ebff522c68
Peter Lyons <pete@peterlyons.com>
2017-11-13 07:53:56 -0700

    Add clap for command line argument parsing

--- tirefox-res-sample/src/main.rs
+++ tirefox-res-sample/src/main.rs
@@ -1,30 +1,52 @@
+#[macro_use]
+extern crate clap;
 extern crate rand;
+use clap::{Arg, App};
+use rand::Rng;
 use std::{fs, io};
 use std::io::prelude::*;
-use rand::Rng;
 
-const HOW_MANY: usize = 20;
 const WORDS_PATH: &str = "/usr/share/dict/words";
 
 fn suitable(word: &str) -> bool {
-    word.len() > 2 && word.len() < 8 && word.chars().nth(0).map(char::is_lowercase).unwrap_or(false)
+    word.len() > 2 && word.len() < 8 &&
+    word.chars()
+        .nth(0)
+        .map(char::is_lowercase)
+        .unwrap_or(false)
 }
 
 fn tirefox() -> io::Result<Vec<String>> {
-    let words_file = fs::File::open(WORDS_PATH)?;
+    let matches = App::new("tirefox")
+        .version(crate_version!())
+        .about("Generate ellisions of random short words")
+        .arg(Arg::with_name("words_path")
+                 .short("w")
+                 .long("words")
+                 .takes_value(true)
+                 // .with_default("/usr/share/dict/words")
+                 .help("Word dictionary path. One word per line."))
+        .arg(Arg::with_name("count")
+                 .short("c")
+                 .long("count")
+                 .takes_value(true)
+                 .help("How many words to generate"))
+        .get_matches();
+    let words_file = fs::File::open(matches.value_of("words_path").unwrap_or(WORDS_PATH))?;
     let words_reader = io::BufReader::new(&words_file);
-    let mut sample = Vec::with_capacity(HOW_MANY);
+    let mut how_many = value_t!(matches, "count", usize).unwrap_or(10) * 2;
+    let mut sample = Vec::with_capacity(how_many);
     for (index, line_result) in words_reader.lines().enumerate() {
         let word = line_result?;
         if !suitable(&word) {
             continue;
         }
-        if sample.len() < HOW_MANY {
+        if sample.len() < how_many {
             // sample is sparse, always fill it initially
             sample.push(word);
         } else {
             let evictee_M = rand::thread_rng().gen_range(0, index + 1);
-            if evictee_M < HOW_MANY {
+            if evictee_M < how_many {
                 sample[evictee_M] = word;
             }
         }
@@ -38,7 +60,7 @@ fn main() {
             for pair in sample.chunks(2) {
                 println!("{}{}", pair[0], pair[1]);
             }
-        },
-        Err(e) => eprintln!("{}",e),
+        }
+        Err(e) => eprintln!("{}", e),
     }
 }
