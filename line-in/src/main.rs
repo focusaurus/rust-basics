3a08a76c8f070f3acecff4a3da9574a991d231da
Peter Lyons <pete@peterlyons.com>
2017-11-13 12:34:50 -0700

    Finish error handling for line-in

--- line-in/src/main.rs
+++ line-in/src/main.rs
@@ -1,32 +1,11 @@
 #[macro_use]
 extern crate clap;
 use clap::{App, Arg};
-use std::env;
+use std::{fs, io, process};
 use std::error::Error;
-// use std::ffi::OsStr;
-use std::{convert, fs, io, process};
 use std::io::ErrorKind;
 use std::io::prelude::*;
 use std::io::SeekFrom;
-use std::path::Path;
-
-enum AnyError {
-    Io(io::Error),
-    Clap(clap::Error),
-}
-
-fn error_exit(message: String, code: i32) {
-    eprintln!("{}", message);
-    std::process::exit(code);
-}
-
-fn bail(err: std::io::Error) {
-    let message = match err.kind() {
-        ErrorKind::NotFound => "Target file not found",
-        _ => err.description(),
-    };
-    error_exit(String::from(message), 10);
-}
 
 fn has_line(target_file: &std::fs::File, to_add: &str) -> bool {
     io::BufReader::new(target_file)
@@ -34,56 +13,7 @@ fn has_line(target_file: &std::fs::File, to_add: &str) -> bool {
         .any(|line| line.unwrap().as_str() == to_add)
 }
 
-fn to_err(message: &str) -> io::Error {
-    io::Error::new(ErrorKind::InvalidData, message)
-}
-
-fn line_in() -> io::Result<i32> {
-    let args = env::args().collect::<Vec<String>>();
-    if args.len() < 3 {
-        let program_name = args.first()
-            .map(Path::new)
-            .and_then(Path::file_name)
-            .map(|o| o.to_string_lossy().into_owned())
-            .unwrap_or_else(|| "UNKNOWN".to_string());
-        error_exit(format!("Usage: {} <target_file> <line_to_add>\n", program_name),
-                   1);
-    }
-    let target_path = &args[1];
-    let to_add = &args[2];
-
-    let target_file = fs::OpenOptions::new()
-        .read(true)
-        .write(true)
-        .open(&target_path)?;
-    if !has_line(&target_file, &to_add) {
-        let mut writer = io::BufWriter::new(target_file);
-        writer.seek(SeekFrom::End(0))?;
-        writeln!(writer, "{}", &to_add);
-        writer.flush()? // possibly unnecessary???
-    }
-    Ok(0)
-}
-
-impl convert::From<clap::Error> for AnyError {
-    fn from(error: clap::Error) -> Self {
-        AnyError::Clap(error)
-    }
-}
-
-
-impl convert::From<io::Error> for AnyError {
-    fn from(error: io::Error) -> Self {
-        AnyError::Io(error)
-    }
-}
-impl convert::From<&'static str> for AnyError {
-    fn from(message: &str) -> Self {
-        AnyError::Io(io::Error::new(ErrorKind::Other, message))
-    }
-}
-
-fn line_in2() -> Result<(), String> {
+fn line_in() -> Result<(), String> {
     let matches = App::new("line-in")
         .version(crate_version!())
         .about("Ensure a file contains a line")
@@ -102,23 +32,24 @@ fn line_in2() -> Result<(), String> {
     let target_file = fs::OpenOptions::new()
         .read(true)
         .write(true)
-        .open(&target_path).map_err(|e|"Error reading target file".to_string())?;
+        .open(&target_path)
+        .map_err(|e| format!("Error reading target file: {}", e))?;
     if !has_line(&target_file, &to_add) {
         let mut writer = io::BufWriter::new(target_file);
-        writer.seek(SeekFrom::End(0)).map_err(|e|"IO error".to_string())?;
-        writeln!(writer, "{}", &to_add);
-        // writer.flush()? // possibly unnecessary???
+        writer
+            .seek(SeekFrom::End(0))
+            .map_err(|e| format!("Error seeking: {}", e))?;
+        writeln!(writer, "{}", &to_add).map_err(|i| i.description().to_string())
+    } else {
+        Ok(())
     }
-    Ok(())
 }
 
 fn main() {
-    match line_in2() {
-        Ok(file) => {
-            println!("line-in OK");
-        }
-        Err(error) => {
-            eprintln!("exiting {}", error);
-            process::exit(1);
-        }
-    }}
+    line_in()
+        .map_err(|error| {
+                     eprintln!("{}", error);
+                     process::exit(1);
+                 })
+        .unwrap();
+}
