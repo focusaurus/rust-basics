adc6ce8143f086afae5b1724f4433df44f6f178d
Peter Lyons <pete@peterlyons.com>
2017-11-15 15:28:13 -0700

    Switched to shaman

--- rustblock/src/main.rs
+++ rustblock/src/main.rs
@@ -1,6 +1,9 @@
-extern crate byte_sha;
+extern crate shaman;
+use shaman::digest::Digest;
 use std::env;
 use std::io::{self, Write};
+
+const DIFFICULTY:u8 = 24;
 
 fn to_bytes(nonce: u32) -> [u8; 4] {
     let mut bytes = [0u8; 4];
@@ -13,14 +16,14 @@ fn to_bytes(nonce: u32) -> [u8; 4] {
     bytes
 }
 
-fn leading_zero_bits(bytes: [u8; 4]) -> u32 {
+fn leading_zero_bits(bytes: [u8; 4]) -> u8 {
     let mut zero_bit_count = 0;
     for orig_byte in &bytes {
         let mut byte = orig_byte.clone();
         // check each of the 8 bits in the byte
         for _bit in 0..8 {
             // check leftmost bit for zeroness
-            if (byte & 128) == 0 {
+            if (byte & 128) == 0 { // 128 in binary is 10000000
                 zero_bit_count += 1;
                 // left shift 1 bit to check the next bit
                 byte = byte << 1;
@@ -41,6 +44,8 @@ fn main() {
     block_vec.append(&mut vec![0, 0, 0, 0]);
     let last_4_index = block_vec.len() - 4;
     let mut nonce: u32 = 0;
+    let mut hasher = shaman::sha2::Sha256::new();
+
     loop {
         let nonce_bytes = to_bytes(nonce);
         // combine block data and nonce into a single slice
@@ -49,10 +54,13 @@ fn main() {
         }
 
         // compute checksum
-        let hash: Vec<u8> = *byte_sha::sha256_of_message_as_u8_vec(&mut block_vec.clone());
+        hasher.reset();
+        hasher.input(&block_vec);
+        let mut hash = [0u8;32];
+        hasher.result(&mut hash);
 
         // check for magic success prefix
-        if leading_zero_bits([hash[0], hash[1], hash[2], hash[3]]) < 24 {
+        if leading_zero_bits([hash[0], hash[1], hash[2], hash[3]]) < DIFFICULTY {
             // nope, increment nonce and loop back around
             if nonce % 1_000_000 == 0 {
                 io::stdout()
@@ -64,11 +72,7 @@ fn main() {
             continue;
         }
 
-        print!("MINED! ");
-        for byte in hash {
-            print!("{:02x}", byte);
-        }
-        println!(" with nonce {}", nonce);
+        println!("MINED! {} with nonce {}", hasher.result_str(), nonce);
         break;
     }
 }
