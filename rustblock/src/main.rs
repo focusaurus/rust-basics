a6deb597649fb4f1a126a1d6c4c9f8a33bf9b796
Peter Lyons <pete@peterlyons.com>
2017-11-17 12:14:25 -0700

    Immutable reference to the payload.

--- rustblock/src/main.rs
+++ rustblock/src/main.rs
@@ -20,19 +20,20 @@ fn block_bytes() -> Vec<u8> {
 
 }
 
-fn mine_nonce(mut payload: Vec<u8>, nonce: u32) -> Answer {
+fn mine_nonce(payload: &Vec<u8>, nonce: u32) -> Answer {
     let len = payload.len();
-    // add 4 bytes space for the nonce at the end of the payload
-    payload.extend([0, 0, 0, 0].iter());
+    // // add 4 bytes space for the nonce at the end of the payload
+    // payload.extend([0, 0, 0, 0].iter());
     let mut hasher = shaman::sha2::Sha256::new();
-    let nonce_bytes = to_bytes(nonce);
+    // let nonce_bytes = to_bytes(nonce);
 
     // combine block data and nonce into a single slice
-    payload[len..len + 4].copy_from_slice(&nonce_bytes);
+    // payload[len..len + 4].copy_from_slice(&nonce_bytes);
 
     // compute checksum
     hasher.reset();
     hasher.input(&payload);
+    hasher.input(&to_bytes(nonce));
     let mut hash = [0u8; 32];
     hasher.result(&mut hash);
 
@@ -51,10 +52,10 @@ fn mine_nonce(mut payload: Vec<u8>, nonce: u32) -> Answer {
     }
 }
 
-fn mine_par<W: io::Write>(mut out: W, mut payload: Vec<u8>) -> io::Result<Block> {
+fn mine_par<W: io::Write>(mut out: W, payload: &Vec<u8>) -> io::Result<Block> {
     let answer = (0u32..u32::max_value())
         .into_par_iter()
-        .map(|nonce| mine_nonce(payload.clone(), nonce))
+        .map(|nonce| mine_nonce(&payload, nonce))
         .find_any(|a| {
             if a.block.is_none() {
                 if a.nonce % 1_000_000 == 0 {
@@ -69,7 +70,7 @@ fn mine_par<W: io::Write>(mut out: W, mut payload: Vec<u8>) -> io::Result<Block>
 }
 
 fn main() {
-    match mine_par(io::stdout(), block_bytes()) {
+    match mine_par(io::stdout(), &block_bytes()) {
         Err(error) => {
             eprintln!("{}", error);
             std::process::exit(10);
--- scratch/src/main.rs
+++ scratch/src/main.rs
@@ -3,7 +3,15 @@
 extern crate rand;
 extern crate shaman;
 use shaman::digest::Digest;
-use std::{env, io, time, thread};
+use std::{env,time, thread};
+use std::io::{self,Write};
+extern crate sha2;
+extern crate digest;
+extern crate digest_writer;
+// use std::fs::File;
+// use std::io::{self, Write};
+// use digest::FixedOutput;
+// use digest_writer::Writer;
 
 fn main() {
     let payload = env::args()
@@ -12,7 +20,11 @@ fn main() {
         .unwrap()
         .to_string()
         .into_bytes();
-    let nonce = "aaa".to_string().into_bytes();
+        // let mut digest = Writer::new(sha2::Sha256::default());
+        // let mut f = File::open("LICENSE-MIT").unwrap();
+        // io::copy(&mut f, &mut digest).unwrap();
+        // digest.fixed_result();
+    let nonce = "aaaa".to_string().into_bytes();
     let mut hasher = shaman::sha2::Sha256::new();
     hasher.input(&payload);
     hasher.input(&nonce);
