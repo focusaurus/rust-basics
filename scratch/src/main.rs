d3cbaedd967d4aa2bb9b241b63b6cb8e9c83e762
Peter Lyons <pete@peterlyons.com>
2017-11-17 10:19:31 -0700

    More realistic scratch miner

--- scratch/src/main.rs
+++ scratch/src/main.rs
@@ -4,18 +4,24 @@ use rand::Rng;
 use rayon::prelude::*;
 use std::{time, thread};
 
-// fn mine_nonce(mut payload: Vec<u8>, nonce: u32) -> Option<String> {
-//
-// }
-fn biz(nonce: u32) -> Option<u32> {
-    let millis = rand::thread_rng().gen_range(0.5, 3.5) * 1000.0;
-    let chance = rand::random::<f32>();
-    println!("checking {} sleep {} chance {}", nonce, millis, chance);
-    thread::sleep(time::Duration::from_millis(millis as u64));
-    if chance >= 0.5 { Some(nonce) } else {None}
+fn mine_nonce(payload: &Vec<u8>, nonce: u32) -> Option<String> {
+    let millis = rand::thread_rng().gen_range(500, 900);
+    let is_golden = rand::random::<f32>() >= 0.99;
+    println!("N: {:>10} G: {:>5} S: {}", nonce, is_golden, millis);
+    thread::sleep(time::Duration::from_millis(millis));
+    if is_golden {
+        Some(format!("block:{}", nonce))
+    } else {
+        None
+    }
 }
 
 fn main() {
-    let nonce = (0u32..11).into_par_iter().find_any(|&i|biz(i).is_some());
-    println!("{:?}", nonce);
+    let mut payload = vec![3, 4];
+    let block = (0u32..u32::max_value())
+        .into_par_iter()
+        .map(|n| mine_nonce(&payload, n))
+        .find_any(|o| o.is_some())
+        .unwrap();
+    println!("{:?}", block);
 }
