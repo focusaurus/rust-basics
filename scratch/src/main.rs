c34d43a26368760a801920c3d79a6361af009b0d
Peter Lyons <pete@peterlyons.com>
2017-11-20 15:23:01 -0700

    Scratch code for filesystem crate

--- scratch/src/main.rs
+++ scratch/src/main.rs
@@ -1,15 +1,21 @@
 extern crate filesystem;
 use filesystem::FileSystem;
 
+fn doit<F>(fs: &F, path: &str) -> String
+    where F: FileSystem
+{
+    let contents = fs.read_file_to_string(path).expect("hey");
+    return contents;
+}
 
 fn main() {
     let fs = filesystem::FakeFileSystem::default();
-    fs.create_dir("/").expect("oops");
-    fs.set_current_dir("/").expect("oops");
+    let fs = filesystem::OsFileSystem::new();
+    fs.create_dir_all("/tmp/scratch").expect("oops");
+    fs.set_current_dir("/tmp/scratch").expect("oops");
     fs.write_file("empty", []).expect("should be writeable");
     fs.write_file("bytes", "inside of bytes".bytes().collect::<Vec<u8>>())
         .expect("should be writeable");
-    let contents = fs.read_file("empty").expect("hey");
-    println!("{:?}", contents.len());
-    println!("{:?}", fs.read_file_to_string("bytes").unwrap());
+    println!("{:?}", doit(&fs, "empty").len());
+    println!("{:?}", doit(&fs, "bytes"));
 }
