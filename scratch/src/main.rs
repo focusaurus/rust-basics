777ec54a64be05748f069f52ca32c711699080d0
Peter Lyons <pete@peterlyons.com>
2017-11-21 12:01:18 -0700

    scratch on rsfs crate

--- scratch/src/main.rs
+++ scratch/src/main.rs
@@ -1,21 +1,18 @@
-extern crate filesystem;
-use filesystem::FileSystem;
-
-fn doit<F>(fs: &F, path: &str) -> String
-    where F: FileSystem
-{
-    let contents = fs.read_file_to_string(path).expect("hey");
-    return contents;
-}
+extern crate rsfs;
+use rsfs::{GenFS,FileType,Metadata};
+use std::io::{Read, Seek, SeekFrom, Write};
 
 fn main() {
-    let fs = filesystem::FakeFileSystem::default();
-    let fs = filesystem::OsFileSystem::new();
-    fs.create_dir_all("/tmp/scratch").expect("oops");
-    fs.set_current_dir("/tmp/scratch").expect("oops");
-    fs.write_file("empty", []).expect("should be writeable");
-    fs.write_file("bytes", "inside of bytes".bytes().collect::<Vec<u8>>())
-        .expect("should be writeable");
-    println!("{:?}", doit(&fs, "empty").len());
-    println!("{:?}", doit(&fs, "bytes"));
+    let fs = rsfs::mem::unix::FS::new();
+    // fs.create_dir("/").expect("oops");
+    // fs.set_current_dir("/").expect("oops");
+    // let _empty = fs.create_file("/empty").unwrap();
+    // , []).expect("should be writeable");
+    let mut bytes = fs.create_file("/bytes").expect("should be writeable");
+    bytes.write(b"inside of bytes").unwrap();
+    // let contents = fs.metadata("/empty").expect("hey");
+    // println!("{:?}", contents);
+    let bytes_meta = fs.metadata("/bytes").unwrap();
+    println!("{:?}", bytes_meta);
+    println!("is_dir{:?}", bytes_meta.is_dir());
 }
