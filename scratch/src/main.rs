ae7427b7c5c341fc96948771c1f709970cbdd371
Peter Lyons <pete@peterlyons.com>
2017-11-27 11:54:25 -0700

    WIP on AsRef

--- scratch/src/main.rs
+++ scratch/src/main.rs
@@ -1,36 +1,19 @@
-extern crate rsfs;
-use rsfs::*;
-use rsfs::unix_ext::*;
-use rsfs::unix_ext::{PermissionsExt, GenFSExt};
+use std::{fmt,path};
 
-fn helper<F: GenFS<Permissions = rsfs::disk::Permissions>>(fs: &rsfs::disk::FS) {
-    // fn helper(fs: &rsfs::disk::FS) {
-    let meta = fs.metadata("/").unwrap();
-    let perms = meta.permissions();
-    // assert_eq!(perms.mode(), 0o755);
-    println!("{:?}", perms.mode());
+#[derive(Debug)]
+struct LearnAsRef<P: AsRef<path::Path>> {
+    path: P,
 }
+
+
+impl<'a, L> fmt::Display for &'a LearnAsRef<AsRef<path::Path>> where L: ?Sized {
+    fn fmt(&self, out: &mut fmt::Formatter) -> fmt::Result {
+        write!(out, "LearnAsRef {}", self.path)
+    }
+}
+
 
 fn main() {
-    let fs = rsfs::mem::unix::FS::new();
-    fs.create_dir_all("/tmp")
-    let file = fs.create_file("/tmp/empty");
-    // let fs = rsfs::mem::unix::FS;
-    // helper::<rsfs::mem::unix::FS>(&fs);
-    helper2::<rsfs::mem::unix::Permissions, rsfs::mem::unix::Metadata, rsfs::mem::unix::FS>(&fs);
-    helper2::<rsfs::disk::Permissions, rsfs::disk::Metadata, rsfs::disk::FS>(&rsfs::disk::FS);
-    helper2(&rsfs::disk::FS);
-    helper2(&fs);
-}
-
-fn helper2<P: Permissions + PermissionsExt,
-           M: Metadata<Permissions = P>,
-           F: GenFS<Permissions = P, Metadata = M>>
-    (fs: &F) {
-    let meta = fs.metadata("/").unwrap();
-    // let perms = M::permissions(&meta);
-    let perms = meta.permissions();
-    // .permissions();
-    // assert_eq!(perms.mode(), 0o755);
-    println!("{:?}", perms.mode());
+    let lar = LearnAsRef { path: path::Path::new("foo") };
+    println!("{:?}", lar);
 }
