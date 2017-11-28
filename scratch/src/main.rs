045b01d91bf21e977b5f8e0dbf30df2a45e16ac9
Peter Lyons <pete@peterlyons.com>
2017-11-28 13:45:10 -0700

    scratch Person sort by age

--- scratch/src/main.rs
+++ scratch/src/main.rs
@@ -1,16 +1,41 @@
 extern crate chrono;
 use chrono::{NaiveDate, Local};
-// use chrono::NaiveDate::{signed_duration_since,from_ymd};
-use std::fmt;
+use std::{cmp, fmt};
 
+#[derive(Eq)]
 struct Person {
     name: String,
     born: NaiveDate,
 }
 
 impl Person {
+    pub fn new(name: String, year: i32, month: u32, day: u32) -> Self {
+        Person {
+            name,
+            born: NaiveDate::from_ymd(year, month, day),
+        }
+    }
+
     pub fn age(&self) -> i64 {
         NaiveDate::signed_duration_since(Local::today().naive_local(), self.born).num_days() / 365
+    }
+}
+
+impl PartialOrd for Person {
+    fn partial_cmp(&self, other: &Person) -> Option<cmp::Ordering> {
+        Some(other.cmp(self))
+    }
+}
+
+impl Ord for Person {
+    fn cmp(&self, other: &Person) -> cmp::Ordering {
+        self.born.cmp(&other.born)
+    }
+}
+
+impl PartialEq for Person {
+    fn eq(&self, other: &Person) -> bool {
+        self.born == other.born
     }
 }
 
@@ -20,15 +45,15 @@ impl fmt::Display for Person {
     }
 }
 
+
+
 fn main() {
-    let people = vec![Person {
-                          name: "Alfred Matthew \"Weird Al\" Yankovic".to_string(),
-                          born: NaiveDate::from_ymd(1959, 10, 23),
-                      },
-                      Person {
-                          name: "Imogen Heap".to_string(),
-                          born: NaiveDate::from_ymd(1977, 12, 9),
-                      }];
+    let mut people = vec![];
+    people.push(Person::new("Imogen Heap".to_string(), 1977, 12, 9));
+    people.push(Person::new("Fatboy Slim".to_string(), 1963, 7, 31));
+    people.push(Person::new("Weird Al".to_string(), 1959, 10, 23));
+    people.push(Person::new("Zoë Keating".to_string(), 1972, 2, 2));
+    people.sort();
     for person in people.iter() {
         println!("{}", person);
     }
