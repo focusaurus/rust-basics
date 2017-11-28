34b7e47006b7e3cd72e97528cf43f89349e86535
Peter Lyons <pete@peterlyons.com>
2017-11-28 12:37:51 -0700

    scratch pem

--- scratch/src/main.rs
+++ scratch/src/main.rs
@@ -1,44 +1,29 @@
-use std::fmt;
+extern crate rsfs;
+extern crate pem;
+use rsfs::GenFS;
+use std::io::Read;
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Clone)]
-pub enum Level {
-    Error,
-    Warning,
-    Ok,
-}
-
-impl fmt::Display for Level {
-    fn fmt(&self, out: &mut fmt::Formatter) -> fmt::Result {
-        write!(out,
-               "{}",
-               match self {
-                   &Level::Error => "🔥",
-                   &Level::Warning => "⚠️",
-                   &Level::Ok => "✓",
-               })
-    }
-}
-
-#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Clone)]
-enum Check {
-    Empty(Level, String),
-    TooSmall(Level, String),
-    TooBig(Level, String),
-}
-
+// ssh-keygen -f test-ssh-key-1 -t ed25519 -C "Tealeves test ED25519 SSH Key 1"
 fn main() {
-    let issues = [Check::Empty(Level::Error, "is empty".to_string()),
-                  Check::TooBig(Level::Warning, "too big".to_string())];
-    println!("{:?}",
-             issues
-                 .iter()
-                 .any(|issue| match issue {
-                          &Check::TooSmall(ref _l, ref _m) => true,
-                          _ => false,
-                      }));
-    // match issue {
-    //     Check::Empty(level, message) |
-    //     Check::TooSmall(level, message) |
-    //     Check::TooBig(level, message) => println!("{}: {}", level, message),
-    // }
+    let fs = rsfs::disk::FS;
+    let mut file = fs.open_file("/tmp/test-ssh-ed25519-1.privatekey")
+        .expect("expect1");
+    let mut key_string = String::new();
+    file.read_to_string(&mut key_string).unwrap();
+    let parsed = pem::parse(key_string).unwrap();
+    // let mut parts: Vec<&str> = key_string.lines().collect();
+    // let footer = parts.pop().unwrap();
+    // let payload: String = parts[1..].join("");
+    // split_whitespace().collect();
+    // let key_type = parts.get(0).unwrap();
+    // let payload = parts.get(1).unwrap();
+    // let comment = parts[2..].join(" ");
+    // print!("{} {}", payload, footer);
+    // println!("{:?}", parsed.contents[0..10].iter().collect::<String>());
+    // println!("{}", String::new(parsed.contents[0..10].into()));
+    let magic = "openssh-key-v1";
+    let found = String::from_utf8(parsed.contents[0..magic.len()].to_vec()).unwrap();
+    println!("{} {}", found, magic == found);
+    let payload_hex: String = parsed.contents.iter().map(|byte| format!("{:x}", byte)).collect();
+    println!("{}: {}", parsed.tag, payload_hex);
 }
