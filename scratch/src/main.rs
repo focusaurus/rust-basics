099c6f66f314e642202919558affc6031a6a5a9e
Peter Lyons <pete@peterlyons.com>
2017-11-28 08:45:27 -0700

    scratch

--- scratch/src/main.rs
+++ scratch/src/main.rs
@@ -1,19 +1,44 @@
-use std::{fmt,path};
+use std::fmt;
 
-#[derive(Debug)]
-struct LearnAsRef<P: AsRef<path::Path>> {
-    path: P,
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Clone)]
+pub enum Level {
+    Error,
+    Warning,
+    Ok,
 }
 
-
-impl<'a, L> fmt::Display for &'a LearnAsRef<AsRef<path::Path>> where L: ?Sized {
+impl fmt::Display for Level {
     fn fmt(&self, out: &mut fmt::Formatter) -> fmt::Result {
-        write!(out, "LearnAsRef {}", self.path)
+        write!(out,
+               "{}",
+               match self {
+                   &Level::Error => "🔥",
+                   &Level::Warning => "⚠️",
+                   &Level::Ok => "✓",
+               })
     }
 }
 
+#[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Clone)]
+enum Check {
+    Empty(Level, String),
+    TooSmall(Level, String),
+    TooBig(Level, String),
+}
 
 fn main() {
-    let lar = LearnAsRef { path: path::Path::new("foo") };
-    println!("{:?}", lar);
+    let issues = [Check::Empty(Level::Error, "is empty".to_string()),
+                  Check::TooBig(Level::Warning, "too big".to_string())];
+    println!("{:?}",
+             issues
+                 .iter()
+                 .any(|issue| match issue {
+                          &Check::TooSmall(ref _l, ref _m) => true,
+                          _ => false,
+                      }));
+    // match issue {
+    //     Check::Empty(level, message) |
+    //     Check::TooSmall(level, message) |
+    //     Check::TooBig(level, message) => println!("{}: {}", level, message),
+    // }
 }
