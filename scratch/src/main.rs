bc032225b1cd4d0cbac9e8faadb3b4011d6f9b68
Peter Lyons <pete@peterlyons.com>
2017-11-28 13:23:48 -0700

    chrono display  person

--- scratch/src/main.rs
+++ scratch/src/main.rs
@@ -1,28 +1,35 @@
-enum Thing {
-    Box, Fox, Socks, Docks
+extern crate chrono;
+use chrono::{NaiveDate, Local};
+// use chrono::NaiveDate::{signed_duration_since,from_ymd};
+use std::fmt;
+
+struct Person {
+    name: String,
+    born: NaiveDate,
 }
 
-#[derive(PartialEq)]
-enum Species {
-    Cat, Dog, Fox, Pig
-}
-
-struct Animal {
-    species: Species
-}
-
-fn is_kind(thing: Thing, kind: Thing) -> bool{
-    match thing {
-        kind => true,
-        _ => false
+impl Person {
+    pub fn age(&self) -> i64 {
+        NaiveDate::signed_duration_since(Local::today().naive_local(), self.born).num_days() / 365
     }
 }
 
-fn is_species(animal: Animal, species: Species) -> bool {
-    animal.species == species
+impl fmt::Display for Person {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} (age: {})", self.name, self.age())
+    }
 }
 
 fn main() {
-    println!("{:?}", is_species(Animal{species: Species::Dog}, Species::Cat));
-    println!("{:?}", is_species(Animal{species: Species::Dog}, Species::Dog));
+    let people = vec![Person {
+                          name: "Alfred Matthew \"Weird Al\" Yankovic".to_string(),
+                          born: NaiveDate::from_ymd(1959, 10, 23),
+                      },
+                      Person {
+                          name: "Imogen Heap".to_string(),
+                          born: NaiveDate::from_ymd(1977, 12, 9),
+                      }];
+    for person in people.iter() {
+        println!("{}", person);
+    }
 }
